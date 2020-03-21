838f4d767438c772e3c821e77b4e8061103a25e8
Harpreet <48261720+Harpreet-Parhar-Canada@users.noreply.github.com>
2020-03-21 15:53:12 -0600

    agaim updation

--- data.R
+++ data.R
@@ -1,2 +1,5 @@
 dataset = read.csv('Data.csv')
-dataset$Age= ifelse(is.na(dataset$Age))
+
+dataset$Age = ifelse(is.na(dataset$Age),
+                  ave(dataset$Age,FUN= function(x) mean(x,na.rm=TRUE)),
+                  dataset$Age)
