0fd7e7187ee5f84c89a7ff6e63baa2d513d3ee89
Harpreet <48261720+Harpreet-Parhar-Canada@users.noreply.github.com>
2020-03-24 13:53:51 -0600

    updation

--- com.py
+++ com.py
@@ -1,14 +1,18 @@
 import csv
 import pandas as pd
 
-arrayForColumns = ['sector','group_category','community_name','count']
-data = pd.read_csv('https://data.calgary.ca/resource/848s-4m4z.csv');
-# deletingColumns =['geocoded_column','resident_count','count']
+# arrayForColumns = ['sector','group_category','community_name','count']
+# data = pd.read_csv('https://data.calgary.ca/resource/848s-4m4z.csv');
+# # deletingColumns =['geocoded_column','resident_count','count']
 
-# dataForEast = df[df['sector'] == 'EAST'].drop(deletingColumns,axis=1);
-# dataForNortheast = df[df['sector'] == 'NORTHEAST'];
-# dataForSouth = df[df['sector'] == 'SOUTH'];
-# dataForWest = df[df['sector'] == 'WEST'];
+# # dataForEast = df[df['sector'] == 'EAST'].drop(deletingColumns,axis=1);
+# # dataForNortheast = df[df['sector'] == 'NORTHEAST'];
+# # dataForSouth = df[df['sector'] == 'SOUTH'];
+# # dataForWest = df[df['sector'] == 'WEST'];
 
-filterData = pd.DataFrame(data,columns=arrayForColumns) 
-print(filterData)
+# filterData = pd.DataFrame(data,columns=arrayForColumns) 
+# print(filterData)
+
+
+data = pd.read_csv("Data.csv")
+print(data)
